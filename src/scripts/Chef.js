593ac01296bda5f8c3e09f055b1845008ba458f6
alexandrelajoie1 <alexlajoie4@gmail.com>
2026-02-26 08:55:02 -0500

    créer le instances de Poutine/arreter a sendOrder/création de la classe Poutine

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -1,10 +1,18 @@
 export default class Chef {
   constructor() {
     this.element;
-    this.menu;
+    this.menu = [];
     this.container;
     this.init();
   }
 
-  init() {}
+  init() {
+    const Poutine = new Poutine();
+    menu.push(Poutine);
+
+    const button = document.querySelector('.button-secondary');
+    button.addEventListener('click', this.sendOrder.bind(this));
+  }
+
+  sendOrder() {}
 }
---
+++ src/scripts/Poutine.js
@@ -0,0 +1,15 @@
+export default class Poutine {
+  constructor() {
+    this.element;
+    this.types;
+    this.selectedType;
+    this.inti();
+  }
+
+  init() {
+    const buttons = document.querySelectorAll('.button');
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+    }
+  }
+}
