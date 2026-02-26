939466070e8145e25087dfc22eb00a7bf8d21ad3
alexandrelajoie1 <alexlajoie4@gmail.com>
2026-02-26 09:30:41 -0500

    update

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,8 +1,8 @@
 export default class Poutine {
   constructor(element) {
     this.element = element;
-    this.types = document.querySelectorAll('.types');
-    this.selectedType;
+    this.types = document.querySelectorAll('.button');
+    this.selectedType = '';
     this.init();
   }
 
@@ -10,16 +10,20 @@ export default class Poutine {
     const buttons = document.querySelectorAll('.button');
     for (let i = 0; i < buttons.length; i++) {
       const button = buttons[i];
-      button.addEventListener('click', this.selectedType.bind(this));
+      button.addEventListener('click', this.selectType.bind(this));
     }
   }
 
-  selectedType(e) {
-    for (let i = 0; i < types.length; i++) {
-      const type = types[i];
+  selectType(e) {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
       type.classList.remove('is-active');
     }
     e.currentTarget.classList.add('is-active');
-    this.selectedType.textContent = `${e.currentTarget}`;
+    this.selectedType = `${e.currentTarget}`;
+    console.log(this.selectedType);
+    this.updatePhoto();
   }
+
+  updatePhoto() {}
 }
